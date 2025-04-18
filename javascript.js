a8a6a03d79601e5d45bdf2ee2de5c4f42c1b44b8
sjfeenstra <sjoerdfeenstra_7@hotmail.com>
2025-04-18 16:10:55 +0200

    Possibility to create a new grid

--- javascript.js
+++ javascript.js
@@ -1,18 +1,32 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
+const resetButton = document.querySelector(".reset-button");
 
-size = 16;
-for(i= 0; i < size; i++){
+const DEFAULT_SIZE = 16;
+
+function createGrid(size) {
+  for (i = 0; i < size; i++) {
     const verticalContainer = document.createElement("div");
     verticalContainer.setAttribute("class", "verticalContainer");
     container.appendChild(verticalContainer);
-    for(j= 0; j < size; j++){
-        const horizontalContainer = document.createElement("div");
-        horizontalContainer.classList.add("horizontalContainer");
-        verticalContainer.appendChild(horizontalContainer);
+    for (j = 0; j < size; j++) {
+      const horizontalContainer = document.createElement("div");
+      horizontalContainer.classList.add("horizontalContainer");
+      verticalContainer.appendChild(horizontalContainer);
 
-        horizontalContainer.addEventListener("mouseover", () =>{
-            horizontalContainer.classList.add("hovered");
-        })
+      horizontalContainer.addEventListener("mouseover", () => {
+        horizontalContainer.classList.add("hovered");
+      });
     }
+  }
 }
 
+createGrid(DEFAULT_SIZE);
+
+resetButton.addEventListener("click", () => {
+    let gridSize = 0;
+    while (gridSize > 100 || gridSize <= 0 || !Number.isInteger(gridSize)) {
+      gridSize = parseInt(prompt("What grid size do you want?"));
+    }
+    container.replaceChildren();
+    createGrid(gridSize)
+  });
